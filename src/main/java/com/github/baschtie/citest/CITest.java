08cccbd341c6b93db1c5590193437e9a926cf55b
Baschtie <Baschtie@users.noreply.github.com>
2020-06-18 14:24:08 +0200

    setup project structure

---
+++ src/main/java/com/github/baschtie/citest/CITest.java
@@ -0,0 +1,13 @@
+package com.github.baschtie.citest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CITest extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
