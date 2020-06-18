06ad13004d2ad8094b6b90e34a047a27cc028c3b
Baschtie <Baschtie@users.noreply.github.com>
2020-06-18 16:17:24 +0200

    add classic code

--- src/main/java/com/github/baschtie/citest/CITest.java
+++ src/main/java/com/github/baschtie/citest/CITest.java
@@ -1,10 +1,12 @@
 package com.github.baschtie.citest;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CITest extends JavaPlugin {
     @Override
     public void onEnable() {
+        Bukkit.getServer().broadcastMessage("Hello World");
     }
 
     @Override
