94c985183302228208a6839894f56e2bebdaee79
Baschtie <Baschtie@users.noreply.github.com>
2020-06-23 20:12:35 +0200

    let test be successfull again

--- src/test/java/com/github/baschtie/TestASimpleMethod.java
+++ src/test/java/com/github/baschtie/TestASimpleMethod.java
@@ -9,6 +9,6 @@ public class TestASimpleMethod {
 
     @Test
     public void testMessage() {
-        assertEquals("§6Hi", MessageBuilder.colorMessageRed("Hi"));
+        assertEquals("§cHi", MessageBuilder.colorMessageRed("Hi"));
     }
 }
